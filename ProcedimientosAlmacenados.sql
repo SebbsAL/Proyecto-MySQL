5770fa391177f983dd0e6f6e68e7e14ab1c7271e
SebbsAL <sebbs0610@gmail.com>
2026-07-13 12:25:22 -0500

    Feat: Script Membresias 2/5

--- ProcedimientosAlmacenados.sql
+++ ProcedimientosAlmacenados.sql
@@ -52,3 +52,41 @@ END //
 DELIMITER ;	
 -- 2. Renovar una membresia existente
 -- Extiende la vigencia de una membresia segun el tipo contratado
+DELIMITER //
+CREATE PROCEDURE RenovarMembresia(
+	IN p_membresia_usuario_id VARCHAR(36),
+	IN p_nueva_fecha_inicio DATE
+)
+BEGIN
+	DECLARE v_duracion INT;
+	DECLARE v_tipo_membresia_id VARCHAR(36);
+	DECLARE v_fecha_fin_actual DATE;
+	DECLARE v_nueva_fecha_fin DATE;
+-- Verificamos si la membresia ya existe y obtenemos sus datos actuales
+	SELECT tipo_membresia_id, fecha_fin
+	INTO v_tipo_membresia_id, v_fecha_fin_actual
+	FROM membresia_usuario
+	WHERE id = p_membresia_usuario_id;
+	IF v_tipo_membresia_id IS NULL THEN
+		SIGNAL SQLSTATE '45000'
+		SET MESSAGE_TEXT = 'Error: La membresia a renovar no existe.';
+	END IF;
+-- Obtenemos la duracion del tipo de membresia para calcularla
+	SELECT duracion_dias INTO v_duracion 
+	FROM tipos_membresia
+	WHERE id = v_tipo_membresia_id;
+-- Calculamos la nueva fecha final de la membresia
+-- Si la membresia no ha vencido, se le suman los dias sobrantes a la nueva fecha final
+-- Se realiza el calculo basandose en la fecha inicial
+	SET v_nueva_fecha_fin = DATE_ADD(p_nueva_fecha_inicio, INTERVAL v_duracion DAY);
+-- Actualizamos la nueva membresia
+	UPDATE membresia_usuario
+	SET fecha_inicio = p_nueva_fecha_inicio,
+		fecha_fin = v_nueva_fecha_fin,
+		estado = 'ACTIVA',
+		renovaciones = renovaciones + 1,
+		fecha_suspension = NULL, -- En caso de que estuviese suspendida, se sanatizan los datos
+		motivo_suspension = NULL 
+	WHERE id = p_membresia_usuario_id;
+END // 
+DELIMITER ;
