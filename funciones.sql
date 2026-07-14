fa7698e9b3441aa1de6df8ba2d0897f72aab6886
JhonHenryReyesPerez <jhonhenryreyesperezstudio@gmail.com>
2026-07-14 17:14:19 -0500

    feat: segunda funcion

--- funciones.sql
+++ funciones.sql
@@ -17,3 +17,28 @@ ELSE
     RETURN FALSE; -- de lo contrario no hay por lo tanto retornara FALSE
 END IF;	
 END;
+-- ================================================================================================================================================
+-- 2. dias restantes membresia
+
+CREATE FUNCTION fn_dias_restantes_membresia(p_usuario_id VARCHAR(36))
+RETURNS INT
+DETERMINISTIC
+READS SQL DATA
+BEGIN
+    DECLARE v_fecha_fin DATE;
+    DECLARE v_dias INT;
+
+    SELECT MAX(fecha_fin) INTO v_fecha_fin -- max lo que hace es q toma todo lo q sobrevio a where y devuelve el -
+    FROM membresia_usuario -- ..mas grande, aunque un usuario no debe tener 2 membresias pero x si acaso
+    WHERE usuario_id = p_usuario_id
+      AND estado = 'ACTIVA';
+
+    IF v_fecha_fin IS NULL THEN
+        SET v_dias = 0; -- no tiene membresía activa, no hay días restantes 
+    ELSE
+        SET v_dias = DATEDIFF(v_fecha_fin, CURDATE()); 
+    END IF;
+
+    RETURN v_dias;
+END;
+
